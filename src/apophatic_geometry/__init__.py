28cad222e88b8afe128729d78c09a25485619680
Novel Byte Labs <novelbytelabs@gmail.com>
2026-08-01 15:13:29 -0400

    Clarify package exports are for MF prototype

--- src/apophatic_geometry/__init__.py
+++ src/apophatic_geometry/__init__.py
@@ -1,4 +1,8 @@
-"""Executable reference model for Apophatic Relational Geometry."""
+"""Executable reference implementation of the ARG M_F feedback prototype.
+
+This package does not yet implement the M_P admissibility projection or the
+combined M_FP model.
+"""
 
 from .model import Parameters, State, derivatives, intrinsic_distance_matrix, intrinsic_edge_lengths
 
